3a3ad69fbf312b9b2ee2cd7f55e2ad28900034d3
Patrick <patrickwiechert@yahoo.com>
2016-04-12 21:12:35 -0500

    Added in controller for Club and all Views

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,31 +1,60 @@
 ### CLUBS ###
 
 # INDEX
-
+get "/clubs" do
+  @clubs = Club.all
+  erb :"clubs/index"
+end
 
 
 # NEW
-
+get "/clubs/new" do
+  @club = Club.new
+  erb :"clubs/new"
+end
 
 
 #CREATE
-
+post "/clubs" do
+  @club = Club.new(params)
+  if @club.save
+    redirect to('/clubs')
+  else
+    erb :"clubs/new"
+  end
+end
 
 
 # SHOW
-
+get "/clubs/:id" do
+  @club = Club.find_by_id(params['id'])
+erb :"clubs/show"
+end
 
 
 #EDIT
-
+get "/clubs/:id/edit" do
+  @club = Club.find_by_id(params['id'])
+  @clubs = Club.all
+  erb :"clubs/edit"
+end
 
 
 # UPDATE
-
+patch "/clubs/:id" do
+  @club = Club.find_by_id(params['id'])
+  if @club.update_attributes(name: params['name'])
+    redirect to("/clubs/#{@club.id}")
+  else
+    erb :"clubs/edit"
+  end
+end
 
 
 # DELETE
-
+# check scratch for delete
+# put in code for reassign_all_clubs
+#  @sponsor.reassign_all_clubs(@new_sponsor)
 
 
 # ALL SPONSORS FOR A CLUB
