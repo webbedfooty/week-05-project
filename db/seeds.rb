9f36e1319fc582abf85f382f8874246247e55d66
Patrick <patrickwiechert@yahoo.com>
2016-04-15 15:21:14 -0500

    Added seeds and not all worked

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,12 @@
 # each of your relevant models.
 #
 # You'll run it with rake db:seed
+
+Club.create(name: "Liverpool", city: "Liverpool")
+#Club.create(name: "Arsenal", city: "London", club_sponsors: "Emirates")
+#Club.create(name: "Fulham", city: "London", club_sponsors: "ForEx")
+Player.create(name: "Messi", value: "10000000")
+Player.create(name: "Zlatan", value: "10,000,000")
+#Player.create(name: "Dalglish", value: "100000", club: "Arsenal")
+Sponsor.create(name: "Aflac")
+#Sponsor.create(name: "Fireball", sponsor_clubs: "Birmingham")
